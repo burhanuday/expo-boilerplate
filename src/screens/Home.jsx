1a249f7d69a41fe89c38d76577c80a17d7b2fe09
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2020-07-26 12:43:14 +0530

    added redux example

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,9 +5,20 @@ import { useSelector, useDispatch } from "react-redux";
 import { incrementCounter } from "@actions/counter";
 
 export default function App() {
+  const counter = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
   return (
     <Surface style={styles.container}>
-      <Subheading>Subheading</Subheading>
+      <Subheading>{JSON.stringify(counter, null, 2)}</Subheading>
+
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => dispatch(incrementCounter(counter + 1))}
+      >
+        Press me
+      </Button>
     </Surface>
   );
 }
