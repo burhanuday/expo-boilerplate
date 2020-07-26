de0d964c14ac768acc7cd131833d4a3d5101913a
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2020-07-26 12:41:01 +0530

    fix module resolver

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,20 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-import { Subheading } from "react-native-paper";
+import { StyleSheet } from "react-native";
+import { Surface, Subheading, Button } from "react-native-paper";
+import { useSelector, useDispatch } from "react-redux";
+import { incrementCounter } from "@actions/counter";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Surface style={styles.container}>
       <Subheading>Subheading</Subheading>
-    </View>
+    </Surface>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.4,
     alignItems: "center",
     justifyContent: "center",
   },
