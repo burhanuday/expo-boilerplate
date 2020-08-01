b52bd10b81a134ee6916a8e25cfd735d8e8cf932
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2020-08-01 17:20:31 +0530

    added color reference

--- theme.js
+++ theme.js
@@ -7,6 +7,37 @@ import {
 } from "react-native-paper";
 import { useColorScheme } from "react-native-appearance";
 
+// colors
+
+// dayMode {
+//   "accent": "#03dac6",
+//   "backdrop": "rgba(0, 0, 0, 0.5)",
+//   "background": "#121212",
+//   "disabled": "rgba(255, 255, 255, 0.38)",
+//   "error": "#CF6679",
+//   "notification": "#ff80ab",
+//   "onBackground": "#FFFFFF",
+//   "onSurface": "#FFFFFF",
+//   "placeholder": "rgba(255, 255, 255, 0.54)",
+//   "primary": "#BB86FC",
+//   "surface": "#121212",
+//   "text": "#ffffff",
+// }
+// nightMode {
+//   "accent": "#03dac4",
+//   "backdrop": "rgba(0, 0, 0, 0.5)",
+//   "background": "#f6f6f6",
+//   "disabled": "rgba(0, 0, 0, 0.26)",
+//   "error": "#B00020",
+//   "notification": "#f50057",
+//   "onBackground": "#000000",
+//   "onSurface": "#000000",
+//   "placeholder": "rgba(0, 0, 0, 0.54)",
+//   "primary": "#6200ee",
+//   "surface": "#ffffff",
+//   "text": "#000000",
+// }
+
 export default function ThemeProvider(props) {
   const scheme = useColorScheme();
 
